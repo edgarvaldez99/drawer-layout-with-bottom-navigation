2fce41e57c5a950a2ff74c45a1195587d9a367f9
Edgar Valdez <edgarvaldez99@gmail.com>
2019-11-20 14:30:50 -0300

    initial commit

---
+++ app/src/main/java/py/com/opentech/drawerwithbottomnavigation/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package py.com.opentech.drawerwithbottomnavigation.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
